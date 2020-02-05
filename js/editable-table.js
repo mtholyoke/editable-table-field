35339db2933a6fd39a0f332aff2197ad46246222
Masha <lifsh22m@mtholyoke.edu>
2020-02-05 12:59:30 -0500

    some changes from december Oo

--- js/editable-table.js
+++ js/editable-table.js
@@ -1,5 +1,13 @@
 // import '@lrnwebcomponents/editable-table/editable-table.js';
-((_) => {
+(($,_) => {
     console.log("hi");
+    $.ajax({
+        url: 'https://drive.google.com/file/d/1XrvDGCuS9MrfYr6kZUgMEUBcNBGoKQFK/view?usp=sharing',
+        dataType: "script",
+        success: (response) => {
+          console.log("yay!", response)
+        }
+      });
+
 })(jQuery, Drupal);
 
